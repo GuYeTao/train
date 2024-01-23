2cf419a88d4f93ab16ba60a777cef8a83ca32105
Mio <1181072169@qq.com>
2024-01-23 21:43:29 +0800

    解决前端对后端Long类型精度丢失的问题

--- member/src/main/java/com/example/train/member/resp/PassengerQueryResp.java
+++ member/src/main/java/com/example/train/member/resp/PassengerQueryResp.java
@@ -1,12 +1,15 @@
 package com.example.train.member.resp;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 
 import java.util.Date;
 
 public class PassengerQueryResp {
+    @JsonSerialize(using= ToStringSerializer.class)
     private Long id;
-
+    @JsonSerialize(using= ToStringSerializer.class)
     private Long memberId;
 
     private String name;
