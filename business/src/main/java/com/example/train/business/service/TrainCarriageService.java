9724f4f2348ea2147642944249a708c82c251495
Mio <1181072169@qq.com>
2024-01-26 21:54:26 +0800

    修改座位页面为只读，修改车厢座位总数为自动计算

--- business/src/main/java/com/example/train/business/service/TrainCarriageService.java
+++ business/src/main/java/com/example/train/business/service/TrainCarriageService.java
@@ -5,6 +5,7 @@ import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjectUtil;
 import com.example.train.business.domain.TrainCarriage;
 import com.example.train.business.domain.TrainCarriageExample;
+import com.example.train.business.enums.SeatColEnum;
 import com.example.train.business.mapper.TrainCarriageMapper;
 import com.example.train.business.req.TrainCarriageQueryReq;
 import com.example.train.business.req.TrainCarriageSaveReq;
@@ -30,6 +31,11 @@ public class TrainCarriageService {
 
     public void save(TrainCarriageSaveReq req) {
         DateTime now = DateTime.now();
+
+        List<SeatColEnum> seatColEnums = SeatColEnum.getColsByType(req.getSeatType());
+        req.setColCount(seatColEnums.size());
+        req.setSeatCount(req.getColCount()*req.getRowCount());
+
         TrainCarriage trainCarriage = BeanUtil.copyProperties(req, TrainCarriage.class);
         if (ObjectUtil.isNull(trainCarriage.getId())) {
             trainCarriage.setId(SnowUtil.getSnowflakeNextId());
