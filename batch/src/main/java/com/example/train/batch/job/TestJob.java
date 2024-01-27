f89fc353394dd6a97b5771b68cfd9ad5a565e15d
Mio <1181072169@qq.com>
2024-01-27 15:59:09 +0800

    禁止定时任务并发执行

--- batch/src/main/java/com/example/train/batch/job/TestJob.java
+++ batch/src/main/java/com/example/train/batch/job/TestJob.java
@@ -4,10 +4,17 @@ import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+//@DisallowConcurrentExecution
 public class TestJob implements Job {
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("testJob");
+        System.out.println("testJob start");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("testJob end");
     }
 }
