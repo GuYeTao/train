0bbdbe078b5f4069ec42faed532744337cec8904
Mio <1181072169@qq.com>
2024-01-29 17:46:07 +0800

    集成OpenFeign实现服务间调用，增加微服务应用名

--- batch/src/main/java/com/example/train/batch/controller/TestController.java
+++ batch/src/main/java/com/example/train/batch/controller/TestController.java
@@ -1,12 +1,18 @@
 package com.example.train.batch.controller;
 
+import com.example.train.batch.feign.BusinessFeign;
+import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Resource
+    BusinessFeign businessFeign;
+
     @GetMapping("/hello")
     public String hello(){
-        return "hello";
+        System.out.println(businessFeign.hello());
+        return "hello11";
     }
 }
---
+++ batch/src/main/java/com/example/train/batch/feign/BusinessFeign.java
@@ -0,0 +1,14 @@
+package com.example.train.batch.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+//@FeignClient("business")
+@FeignClient(name = "business", url = "http://127.0.0.1:8002/business")
+public interface BusinessFeign {
+
+    @GetMapping("hello")
+    String hello();
+
+
+}
