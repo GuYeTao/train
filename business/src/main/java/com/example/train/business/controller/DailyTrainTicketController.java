957ac261171ed8c86b503a2054fd74f594874591
Mio <1181072169@qq.com>
2024-01-30 15:36:37 +0800

    增加用户端余票查询页面（日期，出发地，目的地查询时必须都有）

---
+++ business/src/main/java/com/example/train/business/controller/DailyTrainTicketController.java
@@ -0,0 +1,27 @@
+package com.example.train.business.controller;
+
+import com.example.train.business.req.DailyTrainTicketQueryReq;
+import com.example.train.business.resp.DailyTrainTicketQueryResp;
+import com.example.train.business.service.DailyTrainTicketService;
+import com.example.train.common.resp.CommonResp;
+import com.example.train.common.resp.PageResp;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/daily-train-ticket")
+public class DailyTrainTicketController {
+
+    @Resource
+    private DailyTrainTicketService dailyTrainTicketService;
+
+    @GetMapping("/query-list")
+    public CommonResp<PageResp<DailyTrainTicketQueryResp>> queryList(@Valid DailyTrainTicketQueryReq req) {
+        PageResp<DailyTrainTicketQueryResp> list = dailyTrainTicketService.queryList(req);
+        return new CommonResp<>(list);
+    }
+
+}
---
+++ business/src/main/java/com/example/train/business/controller/StationController.java
@@ -0,0 +1,26 @@
+package com.example.train.business.controller;
+
+import com.example.train.business.resp.StationQueryResp;
+import com.example.train.business.service.StationService;
+import com.example.train.common.resp.CommonResp;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/station")
+public class StationController {
+
+    @Resource
+    private StationService stationService;
+
+    @GetMapping("/query-all")
+    public CommonResp<List<StationQueryResp>> queryList() {
+        List<StationQueryResp> list = stationService.queryAll();
+        return new CommonResp<>(list);
+    }
+
+}
