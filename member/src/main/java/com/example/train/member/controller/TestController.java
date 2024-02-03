b0322c25bd1eefe7cfe89529261fac4f43a04ce4
Mio <1181072169@qq.com>
2024-02-03 16:28:35 +0800

    spring boot集成redis

--- member/src/main/java/com/example/train/member/controller/TestController.java
+++ member/src/main/java/com/example/train/member/controller/TestController.java
@@ -1,6 +1,5 @@
 package com.example.train.member.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 public class TestController {
 
-    @Value("${test.nacos}")
+//    @Value("${test.nacos}")
     private String testName;
 
     @GetMapping("/hello")
