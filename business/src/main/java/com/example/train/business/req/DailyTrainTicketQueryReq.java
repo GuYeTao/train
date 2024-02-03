d7f2fc721a8d6cc82af44dc00e22d4648a558282
Mio <1181072169@qq.com>
2024-02-03 15:41:21 +0800

    使用spring内部缓存Cacheable

--- business/src/main/java/com/example/train/business/req/DailyTrainTicketQueryReq.java
+++ business/src/main/java/com/example/train/business/req/DailyTrainTicketQueryReq.java
@@ -4,6 +4,7 @@ import com.example.train.common.req.PageReq;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
+import java.util.Objects;
 
 public class DailyTrainTicketQueryReq extends PageReq {
 
@@ -58,6 +59,18 @@ public class DailyTrainTicketQueryReq extends PageReq {
 
     public void setEnd(String end) {
         this.end = end;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof DailyTrainTicketQueryReq that)) return false;
+        return Objects.equals(date, that.date) && Objects.equals(trainCode, that.trainCode) && Objects.equals(start, that.start) && Objects.equals(end, that.end) && Objects.equals(((DailyTrainTicketQueryReq) o).getPage(), that.getPage()) && Objects.equals(((DailyTrainTicketQueryReq) o).getSize(), that.getSize());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(date, trainCode, start, end, getPage(), getSize());
     }
 
     @Override
--- business/src/main/java/com/example/train/business/service/DailyTrainTicketService.java
+++ business/src/main/java/com/example/train/business/service/DailyTrainTicketService.java
@@ -24,6 +24,7 @@ import com.github.pagehelper.PageInfo;
 import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -60,6 +61,7 @@ public class DailyTrainTicketService {
         }
     }
 
+    @Cacheable(value = "DailyTrainTicketService.queryList")
     public PageResp<DailyTrainTicketQueryResp> queryList(DailyTrainTicketQueryReq req) {
         DailyTrainTicketExample dailyTrainTicketExample = new DailyTrainTicketExample();
         dailyTrainTicketExample.setOrderByClause("id desc");
