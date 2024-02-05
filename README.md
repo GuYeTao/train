3e382465383dfda7877ef0cd69572a2dc6c055c8
Mio <1181072169@qq.com>
2024-02-05 13:51:16 +0800

    修改README

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # train
 仿12306
+
+### 启动准备：
+
+    redis  -->  redis-server.exe
+    nacos  -->  bin\startup.cmd
+    seata  -->  bin\seata-server.bat
+
+### 项目启动：
+    
+Spring Boot服务启动：
+
+    GatewayApplication
+    BatchApplication
+    BusinessApplication
+    MemberApplication
+
+Vue启动：
+    
+    admin
+    web
--- business/src/main/java/com/example/train/business/service/DailyTrainTicketService.java
+++ business/src/main/java/com/example/train/business/service/DailyTrainTicketService.java
@@ -24,7 +24,6 @@ import com.github.pagehelper.PageInfo;
 import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -61,7 +60,7 @@ public class DailyTrainTicketService {
         }
     }
 
-    @Cacheable(value = "DailyTrainTicketService.queryList")
+//    @Cacheable(value = "DailyTrainTicketService.queryList")
     public PageResp<DailyTrainTicketQueryResp> queryList(DailyTrainTicketQueryReq req) {
         DailyTrainTicketExample dailyTrainTicketExample = new DailyTrainTicketExample();
         dailyTrainTicketExample.setOrderByClause("id desc");
